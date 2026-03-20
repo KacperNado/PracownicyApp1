845cc101af44fde4d08e45d6da3b9fcb7569da1d
Kacper <Kacper@KACPERN>
2026-03-20 20:53:00 +0100

    zad

--- FormAdd/FormAdd.Designer.cs
+++ FormAdd/FormAdd.Designer.cs
@@ -37,13 +37,15 @@
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
+            btnDelete = new Button();
             SuspendLayout();
             // 
             // btnOK
             // 
-            btnOK.Location = new Point(133, 311);
+            btnOK.Location = new Point(116, 233);
+            btnOK.Margin = new Padding(3, 2, 3, 2);
             btnOK.Name = "btnOK";
-            btnOK.Size = new Size(94, 29);
+            btnOK.Size = new Size(82, 22);
             btnOK.TabIndex = 0;
             btnOK.Text = "OK";
             btnOK.UseVisualStyleBackColor = true;
@@ -51,76 +53,93 @@
             // 
             // txtImie
             // 
-            txtImie.Location = new Point(201, 62);
+            txtImie.Location = new Point(176, 46);
+            txtImie.Margin = new Padding(3, 2, 3, 2);
             txtImie.Name = "txtImie";
-            txtImie.Size = new Size(125, 27);
+            txtImie.Size = new Size(110, 23);
             txtImie.TabIndex = 1;
             txtImie.TextChanged += txtImie_TextChanged;
             // 
             // txtNazwisko
             // 
-            txtNazwisko.Location = new Point(201, 105);
+            txtNazwisko.Location = new Point(176, 79);
+            txtNazwisko.Margin = new Padding(3, 2, 3, 2);
             txtNazwisko.Name = "txtNazwisko";
-            txtNazwisko.Size = new Size(125, 27);
+            txtNazwisko.Size = new Size(110, 23);
             txtNazwisko.TabIndex = 2;
             // 
             // txtWiek
             // 
-            txtWiek.Location = new Point(201, 138);
+            txtWiek.Location = new Point(176, 104);
+            txtWiek.Margin = new Padding(3, 2, 3, 2);
             txtWiek.Name = "txtWiek";
-            txtWiek.Size = new Size(125, 27);
+            txtWiek.Size = new Size(110, 23);
             txtWiek.TabIndex = 3;
+            txtWiek.TextChanged += txtWiek_TextChanged;
+            txtWiek.KeyPress += txtWiek_KeyPress;
             // 
             // cmbStanowisko
             // 
             cmbStanowisko.FormattingEnabled = true;
-            cmbStanowisko.Location = new Point(201, 197);
+            cmbStanowisko.Location = new Point(176, 148);
+            cmbStanowisko.Margin = new Padding(3, 2, 3, 2);
             cmbStanowisko.Name = "cmbStanowisko";
-            cmbStanowisko.Size = new Size(151, 28);
+            cmbStanowisko.Size = new Size(133, 23);
             cmbStanowisko.TabIndex = 4;
             cmbStanowisko.SelectedIndexChanged += cmbStanowisko_SelectedIndexChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(73, 62);
+            label1.Location = new Point(64, 46);
             label1.Name = "label1";
-            label1.Size = new Size(38, 20);
+            label1.Size = new Size(30, 15);
             label1.TabIndex = 5;
             label1.Text = "Imie";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(73, 105);
+            label2.Location = new Point(64, 79);
             label2.Name = "label2";
-            label2.Size = new Size(69, 20);
+            label2.Size = new Size(55, 15);
             label2.TabIndex = 6;
             label2.Text = "nazwisko";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(73, 145);
+            label3.Location = new Point(64, 109);
             label3.Name = "label3";
-            label3.Size = new Size(42, 20);
+            label3.Size = new Size(33, 15);
             label3.TabIndex = 7;
             label3.Text = "Wiek";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(73, 200);
+            label4.Location = new Point(64, 150);
             label4.Name = "label4";
-            label4.Size = new Size(84, 20);
+            label4.Size = new Size(67, 15);
             label4.TabIndex = 8;
             label4.Text = "Stanowisko";
             // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(251, 232);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Anuluj";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
             // FormAdd
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(btnDelete);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -130,6 +149,7 @@
             Controls.Add(txtNazwisko);
             Controls.Add(txtImie);
             Controls.Add(btnOK);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "FormAdd";
             Text = "Form1";
             ResumeLayout(false);
@@ -147,5 +167,6 @@
         private Label label2;
         private Label label3;
         private Label label4;
+        private Button btnDelete;
     }
 }
--- FormAdd/FormAdd.cs
+++ FormAdd/FormAdd.cs
@@ -10,7 +10,7 @@ namespace PracownicyApp
         public FormAdd()
         {
             InitializeComponent();
-
+            this.CancelButton = btnDelete;
             cmbStanowisko.Items.Add("Manager");
             cmbStanowisko.Items.Add("Programista");
             cmbStanowisko.Items.Add("Tester");
@@ -30,6 +30,11 @@ namespace PracownicyApp
 
         private void btnOK_Click_1(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtWiek.Text))
+            {
+                MessageBox.Show("Podaj wiek!");
+                return;
+            }
             Imie = txtImie.Text;
             Nazwisko = txtNazwisko.Text;
             Wiek = txtWiek.Text;
@@ -42,6 +47,25 @@ namespace PracownicyApp
         private void cmbStanowisko_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void txtWiek_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtWiek_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
